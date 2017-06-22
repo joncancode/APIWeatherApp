009906f498d1d0f88ffa2eca95966287395da210
Daniel Acquesta <danielacquesta@sbcglobal.net>
2017-06-22 11:53:04 -0700

    added javascript plan

--- weather.js
+++ weather.js
@@ -1,17 +1,54 @@
+//--1-- state
+const state = {
+
+   results: []
+
+}
 
 
-var OPENWEATHER_SEARCH_URL = 'api.openweathermap.org/data/2.5/forecast';
+//--2-- function
+
+//must include http:// protocol
+var OPENWEATHER_SEARCH_URL = 'http://api.openweathermap.org/data/2.5/weather';
 console.log(OPENWEATHER_SEARCH_URL);
 
 function getDataFromApi(searchTerm, callback) {
   var query = {
-    id: searchTerm,
-    APPID: "b518aac4e68754c3d5ef20b99bcc6111",
+    q: searchTerm,
+    appid: "b518aac4e68754c3d5ef20b99bcc6111",
   }
   $.getJSON(OPENWEATHER_SEARCH_URL, query, function(response){
-    console.log(response.items[0]); 
+   console.log(response.name); 
   });
 }
 
+function displayOpenWeatherData(data) {
+  
+  state.results = data.items
+  renderData()
+  
+}
 
-//getDataFromApi();
+//getDataFromApi("london");
+
+
+//--3-- render
+
+function renderData() {
+    console.log("rendering")
+}
+
+console.log("rendering", renderData())
+
+
+//--4-- event handlers
+
+$('.js-search-form').submit(function(event){
+  event.preventDefault();
+
+    var inputElement = $(event.currentTarget).find('.js-query');
+    var inputValue = inputElement.val();
+    getDataFromApi(inputValue, displayOpenWeatherData);
+    inputElement.val("");
+
+})
